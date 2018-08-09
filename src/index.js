848ad966c2691d319226f7adff669e896e253cae
Alexander Smith <AlexMSmithCA@gmail.com>
2018-08-09 14:54:42 -0700

    Start with an imperative verb | Initial implementation (#2)
    * Initial implementation for ensuring a required semver-related label exists on a PR
    
    * Restructuring of source files into src/ folder
    
    * Added unit tests
    
    * (minor) Update 'documentation' label to 'docs' for better backwards compatibility
    
    * Review feedback
    
    * Fix start script
    
    * Read required labels from required-labels.yml where possible
    
    * Post a comment on the PR when required labels are missing

---
+++ src/index.js
@@ -0,0 +1,82 @@
+/** Copyright (c) 2017 Uber Technologies, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+module.exports = robot => {
+  robot.on('pull_request.opened', check);
+  robot.on('pull_request.reopened', check);
+  robot.on('pull_request.edited', check);
+  robot.on('pull_request.synchronize', check);
+  robot.on('pull_request.unlabeled', check);
+  robot.on('pull_request.labeled', check);
+
+  async function check(context) {
+    const {github} = context;
+    const pr = context.payload.pull_request;
+
+    const requiredLabels = (await context.config('required-labels.yml', {
+      labels: [
+        'breaking',
+        'feature',
+        'bugfix',
+        'docs',
+        'discussion',
+        'release',
+        'prerelease',
+        'greenkeeping',
+      ],
+    })).labels;
+
+    // set status to pending while checks happen
+    setStatus(context, {
+      state: 'pending',
+      description:
+        'Checking whether at least one required semver-related label exists',
+    });
+
+    function getRequiredLabels(pr) {
+      const labels = pr.labels;
+      const filteredLabels = labels.filter(l =>
+        requiredLabels.includes(l.name),
+      );
+      return filteredLabels;
+    }
+
+    const hasRequiredLabel = getRequiredLabels(pr).length > 0;
+    if (hasRequiredLabel) {
+      // set status to success
+      return setStatus(context, {
+        state: 'success',
+        description: 'At least one required semver-related label exists',
+      });
+    } else {
+      // failure - missing label
+      github.issues.createComment(
+        context.issue({
+          body: `Please add one of the following required labels:\n- ${requiredLabels.join(
+            '\n- ',
+          )}`,
+        }),
+      );
+
+      return setStatus(context, {
+        state: 'failure',
+        description: `Missing a mandatory semver-related label (e.g. 'breaking' or 'feature')`,
+      });
+    }
+  }
+};
+
+async function setStatus(context, {state, description}) {
+  const {github} = context;
+  return github.repos.createStatus(
+    context.issue({
+      state,
+      description,
+      sha: context.payload.pull_request.head.sha,
+      context: 'probot/pr-label',
+    }),
+  );
+}
