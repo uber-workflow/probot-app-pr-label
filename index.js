c93d9e47da0a943e70eef4d7084aa57eedcd60bb
Ryan Tsao <ryan.j.tsao@gmail.com>
2018-08-21 10:33:01 -0700

    Replace error comments with commit status

--- index.js
+++ index.js
@@ -44,65 +44,19 @@ module.exports = robot => {
       return filteredLabels;
     }
 
-    function generateMissingLabelComment() {
-      return `Please add one of the following required labels:\n- ${requiredLabels.join(
-        '\n- ',
-      )}`;
-    }
-
-    async function getMissingLabelComments() {
-      const userId = process.env.GH_USER_ID;
-      if (userId) {
-        const comments = await github.issues.getComments(context.issue());
-        return comments.data
-          .filter(
-            c =>
-              c.user.id === userId && c.body === generateMissingLabelComment(),
-          )
-          .map(c => c.id);
-      }
-      return [];
-    }
-
-    async function clearComments() {
-      const commentsToDelete = await getMissingLabelComments();
-
-      if (commentsToDelete) {
-        return commentsToDelete.map(id => {
-          return github.issues.deleteComment(
-            context.issue({
-              comment_id: id,
-            }),
-          );
-        });
-      }
-    }
-
     const hasRequiredLabel = getRequiredLabels(pr).length > 0;
     if (hasRequiredLabel) {
-      // delete existing comments, if applicable
-      clearComments();
-
-      // set status to success
-      return setStatus(context, {
+      return void setStatus(context, {
         state: 'success',
         description: 'At least one required semver-related label exists',
       });
-    } else {
-      if (!(await getMissingLabelComments()).length) {
-        github.issues.createComment(
-          context.issue({
-            body: generateMissingLabelComment(),
-          }),
-        );
-      }
-
-      // failure - missing label
-      return setStatus(context, {
-        state: 'failure',
-        description: `Missing a mandatory semver-related label (e.g. 'breaking' or 'feature')`,
-      });
     }
+    return void setStatus(context, {
+      state: 'failure',
+      description: `Missing one of required labels: ${requiredLabels.join(
+        ', ',
+      )}`,
+    });
   }
 };
 
